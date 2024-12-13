1e03d400eff8f7a1b8017545fee34481c57f3753
vi-aponte <victor_aponteb@hotmail.com>
2024-12-13 05:36:09 -0300

    Se inicializa proyecto en Spring Boot

---
+++ src/main/java/com/soap/springbootsoap/SpringbootsoapApplication.java
@@ -0,0 +1,13 @@
+package com.soap.springbootsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soap/springbootsoap/SpringbootsoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap.springbootsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
